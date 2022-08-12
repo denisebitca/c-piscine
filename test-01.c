ea249a1498d3b32cd948d867e30864629b573573
Denise Bitca <denisebitca@42l.fr>
2022-08-12 11:21:33 +0200

    Added test conditions for ex08 of c01

--- test-01.c
+++ test-01.c
@@ -6,7 +6,7 @@
 /*   By: rbitca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 10:30:52 by rbitca            #+#    #+#             */
-/*   Updated: 2022/08/11 16:54:01 by rbitca           ###   ########.fr       */
+/*   Updated: 2022/08/11 17:35:00 by rbitca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 #include "./01/ex04/ft_ultimate_div_mod.c"
 #include "./01/ex05/ft_putstr.c"
 #include "./01/ex07/ft_rev_int_tab.c"
+#include "./01/ex08/ft_sort_int_tab.c"
 #include "./test-libraries.c"
 
 int	main(void)
@@ -108,4 +109,31 @@ int	main(void)
 	test_int(2, t[0], "ex07 - pos1");
 	test_int(5, t[1], "ex07 - pos2");
 	test_int(1, t[2], "ex07 - pos3");
+
+	//TEST ex8
+	
+	int t2[10] = {5, 2, 9, 1, 3, 10, 6, 4, 8, 7};
+
+	ft_sort_int_tab(t2, 10);
+
+	test_int(1, t2[0], "ex08 - pos1");
+
+	test_int(2, t2[1], "ex08 - pos2");
+
+	test_int(3, t2[2], "ex08 - pos3");
+
+	test_int(4, t2[3], "ex08 - pos4");
+
+	test_int(5, t2[4], "ex08 - pos5");
+
+	test_int(6, t2[5], "ex08 - pos6");
+
+	test_int(7, t2[6], "ex08 - pos7");
+
+	test_int(8, t2[7], "ex08 - pos8");
+
+	test_int(9, t2[8], "ex08 - pos9");
+
+	test_int(10, t2[9], "ex08 - pos10");
+
 }
