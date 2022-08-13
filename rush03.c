de7abc0a7dc49b83cb1ab9e634673aab51cbabbb
Oscar nicolas Mozo avella <omozo-av@e2r7p14.clusters.42paris.fr>
2022-08-13 11:40:21 +0200

    rush03norminette

--- rush03.c
+++ rush03.c
@@ -74,9 +74,9 @@ void	rush(int x, int y)
  * */
 void	ft_linepopulate(int x, int y, int i, int j)
 {
-	if (i == 0 || i == y-1)
+	if (i == 0 || i == y - 1)
 	{
-		if (j == 0 )
+		if (j == 0)
 			ft_putchar('A');
 		else if (j == x - 1)
 			ft_putchar('C');
