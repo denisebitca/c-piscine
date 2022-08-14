9e3c538de2b0aa74cee51faa46bde9656adf37fe
Denise Bitca <denisebitca@42l.fr>
2022-08-14 11:32:37 +0200

    LA MOULINETTE

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: rbitca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 08:25:33 by rbitca            #+#    #+#             */
-/*   Updated: 2022/08/12 14:22:15 by rbitca           ###   ########.fr       */
+/*   Updated: 2022/08/14 11:24:13 by rbitca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	while (i != n)
+	while (i != n && src[i])
 	{
-		*dest++ = *src++;
+		dest[i] = src[i];
 		i++;
 	}
+	while (i != n)
+		dest[i] = '\0';
 	return (dest);
 }
--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,29 +6,32 @@
 /*   By: rbitca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 08:25:33 by rbitca            #+#    #+#             */
-/*   Updated: 2022/08/13 17:14:13 by rbitca           ###   ########.fr       */
+/*   Updated: 2022/08/14 11:27:56 by rbitca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str);
+unsigned int	ft_strlen(char *str);
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
 
 	i = 0;
-	while (i != size - 1 && *src)
+	if (size)
 	{
-		*dest++ = *src++;
-		i++;
+		while (i != size - 1 && *src)
+		{
+			*dest++ = *src++;
+			i++;
+		}
 	}
 	*dest = '\0';
 	return (ft_strlen(src));
 }
 
-int	ft_strlen(char *str)
+unsigned int	ft_strlen(char *str)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
 	while (str[i] != '\0')
