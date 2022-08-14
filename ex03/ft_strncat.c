62739a3d99f573bf2b1efc1a5a43117bcfe1154b
Denise Bitca <denisebitca@42l.fr>
2022-08-14 20:49:46 +0200

    Fixed up strncat

--- ex03/ft_strncat.c
+++ ex03/ft_strncat.c
@@ -6,25 +6,28 @@
 /*   By: rbitca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 17:04:03 by rbitca            #+#    #+#             */
-/*   Updated: 2022/08/14 17:22:08 by rbitca           ###   ########.fr       */
+/*   Updated: 2022/08/14 20:46:02 by rbitca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str);
+unsigned int	ft_strlen(char *str);
 
 char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
 	unsigned int	i;
 	unsigned int	srcsize;
 
-	srcsize = ft_strlen(dest);
+	srcsize = ft_strlen(src);
 	i = 0;
-	while (src[i] && i < nb)
+	if (nb)
 	{
-		dest[srcsize + i] = src[i];
-		i++;
+		while (src[i] && i < nb)
+		{
+			dest[srcsize + i] = src[i];
+			i++;
+		}
+		dest[srcsize + i] = '\0';
 	}
-	dest[srcsize + i] = '\0';
 	return (dest);
 }
 
