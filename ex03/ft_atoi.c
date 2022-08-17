d7683291c4b6a2ee3cf3a0a9a9dcdea33c978750
Denise Bitca <denisebitca@42l.fr>
2022-08-17 17:08:01 +0200

    Moulinette fix

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -12,106 +12,46 @@
 
 #include <unistd.h>
 
-int	ft_isspace(char c);
-int	*ft_strnbrlen(char *str, int *ret);
-int	ft_checkchar(char *str, int i, int *sign, int *len);
-int	ft_strcmp(char *s1, char *s2);
-
-int	ft_atoi(char *str)
-{
-	int	strinfo[3];
-	int	i;
-	int	multiplier;
-	int	ret;
-
-	multiplier = 1;
-	if (ft_strcmp(str, "-2147483648") == 0)
-		return (-2147483648);
-	else
-	{
-		ft_strnbrlen(str, strinfo);
-		if (strinfo[2] == 0)
-			return (0);
-		i = strinfo[1] - strinfo[2];
-		while (--strinfo[2])
-			multiplier = multiplier * 10;
-		ret = 0;
-		while (i < strinfo[1] - 1)
-		{
-			ret = ret + ((str[i++] - '0') * multiplier);
-			multiplier = multiplier / 10;
-		}
-		ret = ret + (str[i] - '0');
-	}
-	return (ret * strinfo[0]);
-}
-
-int	ft_checkchar(char *str, int i, int *sign, int *len)
-{
-	if (ft_isspace(str[i]) && *len == 0)
-	{
-		if (i > 0)
-		{
-			if (ft_isspace(str[i - 1]))
-				return (1);
-			else
-				return (0);
-		}
-		return (1);
-	}
-	if ((str[i] == '-' || str[i] == '+') && *len == 0)
-	{
-		if (str[i] == '-')
-			*sign = *sign * -1;
-		return (1);
-	}
-	if (str[i] < '0' || str[i] > '9')
-		return (0);
-	*len = *len + 1;
-	return (1);
-}
-
-int	ft_isspace(char c)
-{
-	if (c == ' ' || c == '\f' || c == '\n')
-		return (1);
-	if (c == '\r' || c == '\t' || c == '\v')
-		return (1);
-	return (0);
-}
-
-int	*ft_strnbrlen(char *str, int *ret)
-{
-	int	i;
-	int	len;
-	int	sign;
-
-	i = -1;
-	len = 0;
-	sign = 1;
-	while (str[++i] != '\0')
-	{
-		if (ft_checkchar(str, i, &sign, &len))
-			continue ;
-		else
-			break ;
-	}	
-	ret[0] = sign;
-	ret[1] = i;
-	ret[2] = len;
-	return (ret);
-}
-
-int	ft_strcmp(char *s1, char *s2)
+int	ft_set_good_start(int *sign, char **str)
 {
 	int	i;
 
 	i = 0;
-	while (s1[i] || s2[i])
-	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+	while (((*str)[i] >= 9 && (*str)[i] <= 13) || (*str)[i] == ' ')
 		i++;
-	}
-	return (0);
+	while ((*str)[i] == '-' || (*str)[i] == '+')
+		if ((*str)[i++] == '-')
+			*sign = -(*sign);
+	(*str) = &((*str)[i]);
+	i = -1;
+	while ((*str)[++i])
+		if ((*str)[i] < '0' || (*str)[i] > '9')
+			break ;
+	return (i);
+}
+
+int	ft_pow(int power)
+{
+	int	ret;
+
+	ret = 1;
+	while (--power)
+		ret *= 10;
+	return (ret);
+}
+
+int	ft_atoi(char *str)
+{
+	int	sign;
+	int	strlen;
+	int	i;
+	int	final;
+
+	sign = 1;
+	i = -1;
+	final = 0;
+	strlen = ft_set_good_start(&sign, &str);
+	while (++i < strlen)
+		final += (str[i] - '0') * ft_pow(strlen - i);
+	return (final * sign);
 }
