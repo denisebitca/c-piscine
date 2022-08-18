ebd029beaf356b3f9dcd04e68c3c579ce07544ca
Denise Bitca <denisebitca@42l.fr>
2022-08-18 08:58:28 +0200

    DONE: ex00

---
+++ ex00/ft_print_program_name.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbitca <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/17 20:25:26 by rbitca            #+#    #+#             */
+/*   Updated: 2022/08/17 20:28:41 by rbitca           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc > 0)
+		write(1, argv[0], ft_strlen(argv[0]));
+	write(1, "\n", 1);
+	return (0);
+}
