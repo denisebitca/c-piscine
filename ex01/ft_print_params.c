4903066078851cc4c11df5959e04013926473a33
Denise Bitca <denisebitca@42l.fr>
2022-08-18 09:53:00 +0200

    DONE: ex01

--- ex01/ft_print_params.c
+++ ex01/ft_print_params.c
@@ -6,21 +6,11 @@
 /*   By: rbitca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 20:29:39 by rbitca            #+#    #+#             */
-/*   Updated: 2022/08/17 20:43:14 by rbitca           ###   ########.fr       */
+/*   Updated: 2022/08/18 09:06:13 by rbitca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	if (str[i])
-		i++;
-	return (i);
-}
 
 int	main(int argc, char **argv)
 {
