b1c1ecb0c621f89209cb61aeab3768d3e1efcec4
Denise Bitca <denisebitca@42l.fr>
2022-08-23 17:39:27 +0200

    Slight changes

--- test08/01.c
+++ test08/01.c
@@ -6,12 +6,13 @@
 /*   By: rbitca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 10:54:23 by rbitca            #+#    #+#             */
-/*   Updated: 2022/08/23 11:00:01 by rbitca           ###   ########.fr       */
+/*   Updated: 2022/08/23 17:28:08 by rbitca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../08/ex01/ft_boolean.h"
 #include <unistd.h>
+#include <stdio.h>
 
 void	ft_putstr(char *str)
 {
@@ -27,6 +28,7 @@ t_bool	ft_is_even(int nbr)
 int	main(int argc, char **argv)
 {
 		(void)argv;
+		printf("%d\n", argc - 1);
 		if (ft_is_even(argc - 1) == TRUE)
 				ft_putstr(EVEN_MSG);
 		else
