c8e781370a41bcb7dc788d9dc5f45842768f583b
Denise Bitca <denisebitca@42l.fr>
2022-08-27 22:59:33 +0200

    Anti Randominette Action

---
+++ ft_nbr_list.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_nbr_list.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbitca <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/27 20:06:17 by rbitca            #+#    #+#             */
+/*   Updated: 2022/08/27 22:59:01 by rbitca           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "num.h"
+#include "list.h"
+#include <stdlib.h>
+
+char	*ft_find_elem_parse(t_list *list, int magnitude)
+{
+	return (NULL);
+}
+
+int	ft_check(unsigned int val, unsigned int m, t_list *list)
+{
+	if (val / 100 != 0)
+	{
+		if (!ft_find_elem_parse(list, val / 100))
+			return (0);
+		if (!ft_find_elem_parse(list, 100))
+			return (0);
+	}
+	if (val % 100 >= 20)
+	{
+		if (!ft_find_elem_parse(list, (val % 100) - (val % 10)))
+			return (0);
+		if (val % 10 > 0 && !ft_find_elem_parse(list, val % 10))
+			return (0);
+	}
+	else if (val % 100 > 0 && !ft_find_elem_parse(list, val % 100))
+			return (0);
+	if (m != 1 && !ft_find_elem_parse(list, magnitude))
+			return (0);
+	return (1);
+}
+
+int	ft_check_itow(unsigned int nb, t_list *list)
+{
+	unsigned int	val;
+	unsigned int	magnitude;
+
+	magnitude = 1000000000;
+
+	while (magnitude && nb)
+	{
+		val = nb / magnitude;
+		if (val)
+		{
+			nb = nb % magnitude;
+			if (!ft_check_itow(val, magnitude, list))
+				return (0);			
+		}
+		magnitude /= 1000;
+	}
+	return (1);
+}
+
+void	ft_itow_helper(unsigned int val, unsigned int m, t_list *list)
+{
+	if (val / 100 != 0)
+	{
+		ft_putstr(ft_find_elem_parse(list, val / 100));
+		//space
+		ft_putstr(ft_find_elem_parse(list, 100));
+	}
+	if (val % 100 >= 20)
+	{
+		ft_putstr(ft_find_elem_parse(list, (val % 100) - (val % 10)));
+		if (val % 10 > 0)
+			//space
+			ft_putstr(ft_find_elem_parse(list, val % 10));
+	}
+	else if (val % 100 > 0)
+		//space
+		ft_putstr(ft_find_elem_parse(list, val % 100));
+	if (m != 1)
+		//space
+		ft_find_elem_parse(list, m);
+}
+
+int	ft_itow(unsigned int nb, t_list *list)
+{
+	unsigned int	val;
+	unsigned int	magnitude;
+
+	magnitude = 1000000000;
+
+	while (magnitude && nb)
+	{
+		val = nb / magnitude;
+		if (val)
+		{
+			nb = nb % magnitude;
+			ft_itow_helper(val, magnitude, list);
+		}
+		magnitude /= 1000;
+	}
+	return (1);
+}
--- list.h
+++ list.h
@@ -5,58 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rbitca <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/27 16:10:01 by rbitca            #+#    #+#             */
-/*   Updated: 2022/08/27 17:54:26 by rbitca           ###   ########.fr       */
+/*   Created: 2022/08/27 20:07:57 by rbitca            #+#    #+#             */
+/*   Updated: 2022/08/27 22:35:43 by rbitca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIST_H
 # define LIST_H
 
-/*
-	magnitude_tens can only be between 20 and 90 in
-	steps of ten (20, 30, 40, 50, 60, 70, 80 and 90)
-	(corresponds with the English between twenty and ninty) OR 0.
-	
-	unit_tens can only be between 1 and 19 if magnitude_tens = 0
-	OR between 0 and 9 if magnitude_tens is between 20 and 90.
-*/
-
-typedef struct	s_tens
+typedef struct	s_list
 {
-	int		magnitude_tens;
-	int		unit_tens;
-}				t_tens;
+	int		magnitude;
+	char	*words;
+	t_list	*next;
+}				t_list;
 
-/*
-	unit_hundreds can only be between 0 and 9. If it's equal to 0,
-	there is no number in the hundreds, but there are tens!
-
-	If tens is not set (== NULL), there are no tens.
-	If unit_hundreds = 0, there are tens.
-
-	tens cannot be null if unit_hundreds is null and
-	vice-versa.
-*/
-
-typedef struct	s_hundreds
-{
-	int		unit_hundreds;
-	t_tens	*tens;
-}				t_hundreds;
-
-/*
-	unit_full must always be set. If it is not set, something
-	is seriously wrong with my code.
-
-
-*/
-
-typedef struct	s_fullnum
-{
-	t_hundreds	unit_full;
-	int			magnitude_full;
-	t_fullnum	*next;
-}				t_fullnum;
+t_list	*ft_create_elem_parse(int magnitude, char *words);
+void	ft_push_elem_parse_last(t_list **begin_list, int magnitude, char *words);
+char	*ft_find_elem_parse(t_list *list, int magnitude);
 
 #endif
