8c6838b0e4e1529c05b2dd8cf7c7614acc3ac069
Denise Bitca <denisebitca@42l.fr>
2022-08-29 17:22:16 +0200

    Started work on map_printer

--- src/map_handling/map_printer.c
+++ src/map_handling/map_printer.c
@@ -6,7 +6,7 @@
 /*   By: aronez <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 16:06:15 by aronez            #+#    #+#             */
-/*   Updated: 2022/08/29 16:06:15 by aronez           ###   ########.fr       */
+/*   Updated: 2022/08/29 17:21:48 by rbitca           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,18 @@
 
 int	map_printer(t_map map, t_coords tlcorner, int size, char **char_map)
 {
-	(void)map;
-	(void)tlcorner;
-	(void)size;
-	(void)char_map;
-	return (-1);
+	int	x;
+	int	y;
+	int	i;
+	int	j;
+	int	pos;
+
+	x = map.dimensions.x;
+	y = map.dimensions.y;
+	i = 1;
+	j = 1;
+	*char_map = malloc(sizeof(char) * (x + 1) * (y + 1));
+	if ((*char_map) == NULL)
+		return (-1);
+	return (0);
 }
