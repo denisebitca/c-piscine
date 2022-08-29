57f813ea5f1aede596d672b79b86007b4e8eaf69
aronez <aronez@e3r10p8.clusters.42paris.fr>
2022-08-29 17:18:12 +0200

    Completed the maximum square algorithm. Changed the test file to be formatted properly.

--- src/find_square/find_square.c
+++ src/find_square/find_square.c
@@ -12,10 +12,66 @@
 
 #include "src/map.h"
 
+int	min(int a, int b, int c)
+{
+	if (a <= b)
+	{
+		if (a <= c)
+			return (a);
+		else
+			return (c);
+	}
+	else
+	{
+		if (a <= c)
+			return (c);
+		else
+			return (a);
+	}
+}
+
 int	find_largest_square(t_map map, t_coords *tl_corner, int *square_size)
 {
-	(void)map;
-	(void)tl_corner;
-	(void)square_size;
-	return (-1);
+	int	i;
+	int	j;
+	int *cell;
+
+	i = -1;
+	while (++i < map.dimensions.x)
+	{
+		j = -1;
+		while (++j < map.dimensions.y)
+		{
+			cell = &map.heat_map[(i + 1) + (j + 1) * (map.dimensions.x + 2)];
+			if (*cell != -1)
+			{
+				*cell = 1 + min(
+						map.heat_map[(i + 0) + (j + 0) * (map.dimensions.x + 2)],
+						map.heat_map[(i + 0) + (j + 1) * (map.dimensions.x + 2)],
+						map.heat_map[(i + 1) + (j + 0) * (map.dimensions.x + 2)]
+				);
+			}
+		}
+	}
+	
+	*square_size = 0;
+	i = -1;
+	while (++i < map.dimensions.x)
+	{
+		j = -1;
+		while (++j < map.dimensions.y)
+		{
+			cell = &map.heat_map[(i + 1) + (j + 1) * (map.dimensions.x + 2)];
+			if (*cell >= *square_size)
+			{
+				*square_size = *cell + 1;
+				tl_corner->x = i - (*square_size - 1);
+				tl_corner->y = j - (*square_size - 1);
+			}
+		}
+	}
+	
+	if (*square_size == 0)
+		return (-1);
+	return (0);
 }
